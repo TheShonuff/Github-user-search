506d3bf19faad29da54c256544aa2e4291d48b91
TheShonuff <jirvine124@gmail.com>
2022-05-08 21:45:26 -0700

    Major Styling Blocked Out

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect } from "react";
 import UserView from "./UserView";
 import "./Search.css";
+import search from "./assets/icon-search.svg";
 
 class Search extends React.Component {
   constructor(props) {
@@ -36,26 +37,58 @@ class Search extends React.Component {
       );
   }
 
+  componentDidMount() {
+    console.log("Component Mounted and starting Load...");
+    fetch("https://api.github.com/users/Octocat")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: false,
+            error,
+          });
+        }
+      );
+  }
+
   render() {
     const { erro, isLoaded, data } = this.state;
     console.log(data);
     return (
-      <div className="Searchbar">
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            id="search"
-            name="search"
-            placeholder="Search Github username..."
-          ></input>
-          <button type="submit">Search</button>
-        </form>
+      <div className="Search">
+        <div className="Search-bar">
+          <img src={search}></img>
+          <form className="Search-Form" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search Github username..."
+            ></input>
+            <button id="submit" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
         <UserView
           avatar={data.avatar_url}
           name={data.name}
           login={data.login}
           bio={data.bio}
           created={data.created_at}
+          repos={data.public_repos}
+          followers={data.followers}
+          following={data.following}
+          company={data.company}
+          twitter={data.twitter_username}
+          location={data.location}
+          website={data.blog}
         />
       </div>
     );
--- src/UserView.js
+++ src/UserView.js
@@ -1,6 +1,10 @@
 import React, { Component, useDebugValue } from "react";
 import Search from "./Search";
 import "./UserView.css";
+import locationSVG from "./assets/icon-location.svg";
+import twitterSVG from "./assets/icon-twitter.svg";
+import websiteSVG from "./assets/icon-website.svg";
+import companySVG from "./assets/icon-company.svg";
 
 class UserView extends React.Component {
   constructor(props) {
@@ -8,17 +12,55 @@ class UserView extends React.Component {
     this.state = {};
   }
   render() {
+    let stringBio = "";
+    if (this.props.bio === null) {
+      stringBio = "This profile has no bio";
+    } else {
+      stringBio = this.props.bio;
+    }
     return (
       <div className="UserView">
         <div className="UserTop">
           <img src={this.props.avatar}></img>
           <div className="UserInfo">
-            <h3 className="Name">{this.props.name}</h3>
+            <div className="container">
+              <h3 className="Name">{this.props.name}</h3>
+              <p className="Joined">{this.props.created}</p>
+            </div>
 
             <p className="Login">{this.props.login}</p>
-            <p className="Bio">{this.props.bio}</p>
+            <p className="Bio">{stringBio}</p>
           </div>
-          <p className="Joined">{this.props.created}</p>
+        </div>
+        <div className="repoInfo">
+          <div className="repoTitles">
+            <p>Repos</p>
+            <p>Followers</p>
+            <p>Following</p>
+          </div>
+          <div className="repoCount">
+            <p>{this.props.repos}</p>
+            <p>{this.props.followers}</p>
+            <p>{this.props.following}</p>
+          </div>
+        </div>
+        <div className="contactInfo">
+          <div className="location">
+            <img src={locationSVG}></img>
+            <p>{this.props.location}</p>
+          </div>
+          <div className="twitter">
+            <img src={twitterSVG}></img>
+            <p>{this.props.twitter}</p>
+          </div>
+          <div className="website">
+            <img src={websiteSVG}></img>
+            <p>{this.props.blog}</p>
+          </div>
+          <div className="company">
+            <img src={companySVG}></img>
+            <p>{this.props.company}</p>
+          </div>
         </div>
       </div>
     );
